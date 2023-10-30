a6048cdd04de663fdbb2451cc7d2cfbda452492f
kyumho kim <rlaruagh030@gmail.com>
2023-10-30 09:36:59 +0900

    feat : 프로젝트 생성

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Calc {
+
+    public int run(String op) {
+        return 0;
+    }
+
+    private int multiply(int a, int b) {
+        return a * b;
+    }
+
+    private int divide(int a, int b) {
+        return a / b;
+    }
+
+    private int add(int a, int b) {
+        return a + b;
+    }
+
+    private int minus(int a, int b) {
+        return a - b;
+    }
+}
